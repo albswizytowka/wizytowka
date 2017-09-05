7039dbfe38b102c8885ef6a5cccd579295905952
albswizytowka <31659438+albswizytowka@users.noreply.github.com>
2017-09-05 15:49:59 +0200

    initial

---
+++ inc/main/scripts.php
@@ -0,0 +1,3 @@
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="js/lib/jquery.min.js"><\/script>')</script>
+	<script src="js/all.min.js?v=<?= filemtime(__DIR__ . '/../../www/js/all.min.js') ?>"></script>
